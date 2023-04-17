804a9c2f3e08f7d60d77436856d63bbdd9f15e68
Samih saad <116285682+Bakka08@users.noreply.github.com>
2023-04-17 10:19:56 +0200

    a

--- drivebackend/backend/backend/urls.py
+++ drivebackend/backend/backend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from drive.views import login
+from drive.views import login,adminlogin
 from drive.views import show_all_users
 
 urlpatterns = [
       path('admin/', admin.site.urls),
       path('login/<str:email>/<str:password>/', login, name='login'),
+      path('adminlogin/<str:email>/<str:password>/', adminlogin, name='adminlogin'),
       path('users/', show_all_users, name='show_all_users'),
 ]
 
--- drivebackend/backend/drive/views.py
+++ drivebackend/backend/drive/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 from .models import user
+from .models import admin
 from django.views.decorators.csrf import csrf_exempt
 
 def show_all_users(request):
@@ -23,13 +24,29 @@ def login(request, email, password):
         user_obj = user.objects.get(email=email)
     except user.DoesNotExist:
         # If the user doesn't exist, return an error response
-        return JsonResponse({"success": False, "message": "Invalid email or password."})
+        return JsonResponse({"success": False})
 
     # Check if the password is correct
     if user_obj.password == password:
         # If the password is correct, return a success response
-        return JsonResponse({"success": True, "message": "Login successful."})
+        return JsonResponse({"success": True, })
     else:
         # If the password is incorrect, return an error response
-        return JsonResponse({"success": False, "message": "Invalid email or password."})
+        return JsonResponse({"success": False, })
+    
+@csrf_exempt
+def adminlogin(request, email, password):
+    try:
+        # Attempt to retrieve the user with the given email
+        admin_obj = admin.objects.get(email=email)
+    except admin.DoesNotExist:
+        # If the user doesn't exist, return an error response
+        return JsonResponse({"success": False})
 
+    # Check if the password is correct
+    if admin_obj.password == password:
+        # If the password is correct, return a success response
+        return JsonResponse({"success": True, })
+    else:
+        # If the password is incorrect, return an error response
+        return JsonResponse({"success": False, })
