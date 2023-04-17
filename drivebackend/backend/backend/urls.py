f540051e7bddce0cdc3794f2abedce5658b3c024
Bakka08 <samihsaadlo@gmail.com>
2023-04-17 01:54:48 +0000

    a

--- drivebackend/backend/backend/urls.py
+++ drivebackend/backend/backend/urls.py
@@ -5,7 +5,7 @@ from drive.views import show_all_users
 
 urlpatterns = [
       path('admin/', admin.site.urls),
-       path('login/<str:email>/<str:password>/', login, name='login'),
+      path('login/<str:email>/<str:password>/', login, name='login'),
       path('users/', show_all_users, name='show_all_users'),
 ]
 
