22e9e1ed83361e526f785577bf21ada6d19ad531
Bakka08 <samihsaadlo@gmail.com>
2023-04-28 17:35:04 +0100

    committed

--- drivebackend/backend/backend/urls.py
+++ drivebackend/backend/backend/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from drive.views import show_all_users , login , signup
+from drive.views import show_all_users , login , signup ,search
 
 urlpatterns = [
       path('admin/', admin.site.urls),
       path('users/', show_all_users, name='show_all_users'),
        path('login/', login, name='login'),
         path('signup/', signup.as_view(), name='signup'),
+        path('search/', search, name='search'),
 ]
 
 
--- drivebackend/backend/drive/views.py
+++ drivebackend/backend/drive/views.py
@@ -53,7 +53,7 @@ from .models import user
 
 class signup(APIView):
     def post(self, request):
-        # retrieve data from the request body
+        
         fname = request.data.get('fname')
         lname = request.data.get('lname')
         email = request.data.get('email')
@@ -61,7 +61,7 @@ class signup(APIView):
         password = request.data.get('password')
         is_admin = request.data.get('is_admin', False)
 
-        # create a new User object
+        
         new_user = user(
             fname=fname,
             lname=lname,
@@ -71,10 +71,47 @@ class signup(APIView):
             is_admin=is_admin
         )
 
-        # save the user object to the database
+        
         new_user.save()
 
-        # return a success response
+        
         return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
 
 
+from django.http import JsonResponse
+from .models import voiture, reservation
+from datetime import datetime
+
+def search(request):
+    date1 = request.GET.get('date1').strip()
+    date2 = request.GET.get('date2').strip()
+
+    date1 = datetime.strptime(date1, '%Y-%m-%d').date()
+    date2 = datetime.strptime(date2, '%Y-%m-%d').date()
+
+    cars = voiture.objects.all()
+    reservations = reservation.objects.all()
+
+    
+    valid_reservations = [r for r in reservations if date2 >= r.date_debut and date1 <= r.date_fin]
+
+    available_cars = []
+    for car in cars:
+        car_has_reservation = any(r.voiture.id == car.id for r in valid_reservations)
+        if not car_has_reservation:
+            available_cars.append({
+                'id': car.id,
+                'mark': car.mark,
+                'type': car.type,
+                'image': car.image,
+                'Mileage': car.Mileage,
+                'Transmission': car.Transmission,
+                'Seats': car.Seats,
+                'Luggage': car.Luggage,
+                'Fuel': car.Fuel,
+                'price': car.price
+            })
+
+    data = {'cars': available_cars}
+
+    return JsonResponse(data)
